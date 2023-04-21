155e05713a1b2049f79a022e6e461d0e7a330dfe
Cristiano-3 <chunanluo@126.com>
2023-04-21 18:45:29 +0800

    update

--- tools/main.py
+++ tools/main.py
@@ -2,11 +2,12 @@
 Author: Cristiano-3 chunanluo@126.com
 Date: 2023-03-20 14:24:05
 LastEditors: Cristiano-3 chunanluo@126.com
-LastEditTime: 2023-04-20 18:46:48
+LastEditTime: 2023-04-21 14:12:35
 FilePath: /SVTR/tools/train.py
 Description: 
 '''
 # coding: utf-8
+import os
 import time
 import yaml
 import argparse
@@ -22,6 +23,7 @@ from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel
 
+from modeling.architecture.rec_model import RecModel
 from datasets.rec_dataset import RecDataset
 from utils import AverageMeter, ProgressMeter, Summary
 from modeling.metrics.rec_metric import accuracy
@@ -118,52 +120,88 @@ def validate(val_loader, model, criterion, device, config):
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train SVTR Text Recognition Model.')
-    parser.add_argument('--cfg', help='configuration file name.', required=True, type=str)
-    parser.add_argument('--freq', help='frequency of logging.', default=10, type=int)
-    parser.add_argument('--gpu', help='gpus', type=str)
-    parser.add_argument('--worker', help='num of dataloader workers', type=int)
+    parser.add_argument('-c', '--cfg', help='configuration file name.', required=True, type=str)
+    # parser.add_argument('--freq', help='frequency of logging.', default=10, type=int)
+    # parser.add_argument('--gpu', help='gpus', type=str)
+    # parser.add_argument('--worker', help='num of dataloader workers', type=int)
     
     args = parser.parse_args()
     return args
 
-def main():
-    random.seed(23)
-    torch.manual_seed(23)
-    np.random.seed(23)
-    
+def main():    
     # parse configuration
     args = parse_args()
     with open(args.cfg, 'r') as f:
         config = yaml.safe_load(f)
         config = edict(config)
 
-    # cudnn.benchmark = config.CUDNN.BENCHMARK
-    # cudnn.deterministic = config.CUDNN.DETERMINISTIC
-    # cudnn.enabled = config.CUDNN.ENABLED
-    cudnn.benchmark = True      # benchmark: find best conv alg
-    cudnn.deterministic = False # deterministic operations
-    cudnn.enabled = True
+    # set seeds
+    if config.SEED is not None:
+        random.seed(config.SEED)
+        np.random.seed(config.SEED)
+        torch.manual_seed(config.SEED)
+        # torch.cuda.manual_seed(config.SEED)
+        # torch.cuda.manual_seed_all(config.SEED)
 
-    # 
-    torch.distributed.is_nccl_available()
+        cudnn.benchmark = False
+        cudnn.deterministic = True
+        cudnn.enabled = False
+    else:
+        # cudnn.benchmark = config.CUDNN.BENCHMARK
+        # cudnn.deterministic = config.CUDNN.DETERMINISTIC
+        # cudnn.enabled = config.CUDNN.ENABLED
+        cudnn.benchmark = True      # benchmark: find best conv alg
+        cudnn.deterministic = False # deterministic operations
+        cudnn.enabled = True
+
     dist.init_process_group(backend=dist.Backend.NCCL)
 
-    if args.gpu is not None:
-        config.GPUS = args.gpu
-        print("Use GPU: {} for training".format(args.gpu))
-
     # training device
-    if config.GPUS is not None and torch.cuda.is_available():
-        local_rank = torch.distributed.
+    if torch.cuda.is_available():
+        local_rank = torch.distributed.get_rank()
         torch.cuda.set_device(local_rank)
         device = torch.device("cuda", local_rank)
+
     elif torch.backends.mps.is_available():
         # (GPU for MacOS devices with Metal programming framework)
         device = torch.device("mps")    
     else:
         device = torch.device("cpu")
+    
+    criterion = Loss
+    optimizer = Optimizer
+    scheduler = Scheduler
 
+    # create & load model
+    model = RecModel(config)    
     # resume from a checkpoint
+    resume_file = config.TRAIN.RESUME.FILE
+    if resume_file:
+        if os.path.isfile(resume_file):
+            print(f"loading checkpoint {resume_file}")
+            if torch.cuda.is_available():
+                checkpoint = torch.load(resume_file, map_location=f'cuda:{local_rank}')
+            else:
+                checkpoint = torch.load(resume_file)
+            config.START_EPOCH = checkpoint['epoch']
+            best_acc = checkpoint['best_acc']
+            if torch.cuda.is_available():
+                best_acc.to(device)
+            
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            scheduler.load_state_dict(checkpoint['scheduler'])
+            
+            print(f"=> loaded checkpoint {resume_file} (epoch {checkpoint['epoch']})")
+        else:
+            print(f"=> no checkpoint found at {resume_file}")
+
+    # model.to(device)
+    model = DistributedDataParallel(
+            model, 
+            device_ids=[local_rank], 
+            output_device=local_rank
+            )
 
     # data loading
     trainset = RecDataset("", "")
@@ -186,12 +224,9 @@ def main():
         pin_memory=config.PIN_MEM
     )
 
-    model = DistributedDataParallel(model, device_ids=[0])
-    criterion = Loss
-    optimizer = Optimizer
-
     # train
     for epoch in range(config.START_EPOCH, config.EPOCHS):
+        trainsampler.set_epoch(epoch)
         train(trainloader, model, criterion, optimizer, epoch, device, config)
         validate(valloader, model, criterion, config)
 
