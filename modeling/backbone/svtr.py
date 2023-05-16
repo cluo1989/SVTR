a2cff9491268022b25888f78bee08df1db842e17
C. Luo <chunanluo@126.com>
2023-05-16 11:24:36 +0800

    comment init weights in ConvBNLayer

--- modeling/backbone/svtr.py
+++ modeling/backbone/svtr.py
@@ -43,28 +43,28 @@ class ConvBNLayer(nn.Module):
             bias=bias)
         self.bn = nn.BatchNorm2d(out_channel)
         self.act = act()
-        self.apply(self._init_weights)
+    #     self.apply(self._init_weights)
 
-    def _init_weights(self, m):
-        # weight initialization
-        if isinstance(m, nn.Conv2d):
-            kaiming_normal_(m.weight, mode='fan_out')
-            if m.bias is not None:
-                zeros_(m.bias)
-        elif isinstance(m, nn.BatchNorm2d):
-            ones_(m.weight)
-            zeros_(m.bias)
-        elif isinstance(m, nn.Linear):
-            nn.init.normal_(m.weight, 0, 0.01)
-            if m.bias is not None:
-                zeros_(m.bias)
-        elif isinstance(m, nn.ConvTranspose2d):
-            kaiming_normal_(m.weight, mode='fan_out')
-            if m.bias is not None:
-                zeros_(m.bias)
-        elif isinstance(m, nn.LayerNorm):
-            ones_(m.weight)
-            zeros_(m.bias)
+    # def _init_weights(self, m):
+    #     # weight initialization
+    #     if isinstance(m, nn.Conv2d):
+    #         kaiming_normal_(m.weight, mode='fan_out')
+    #         if m.bias is not None:
+    #             zeros_(m.bias)
+    #     elif isinstance(m, nn.BatchNorm2d):
+    #         ones_(m.weight)
+    #         zeros_(m.bias)
+    #     elif isinstance(m, nn.Linear):
+    #         nn.init.normal_(m.weight, 0, 0.01)
+    #         if m.bias is not None:
+    #             zeros_(m.bias)
+    #     elif isinstance(m, nn.ConvTranspose2d):
+    #         kaiming_normal_(m.weight, mode='fan_out')
+    #         if m.bias is not None:
+    #             zeros_(m.bias)
+    #     elif isinstance(m, nn.LayerNorm):
+    #         ones_(m.weight)
+    #         zeros_(m.bias)
 
     def forward(self, inputs):
         out = self.conv2d(inputs)
